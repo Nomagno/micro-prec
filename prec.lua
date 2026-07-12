80aaa256bfd199cbbeddb395a6963073835555a8
Nomagno <alvarosaez316@gmail.com>
2026-07-13 01:39:11 +0200

    Fix load order

--- prec.lua
+++ prec.lua
@@ -1,7 +1,9 @@
 VERSION = "1.0.0"
 config = import("micro/config")
 
-function init()
+function preinit()
     config.AddRuntimeFile("prec", config.RTSyntax, "prec.yaml")
-    linter.makeLinter("precc", "prec", "precc", {"-fsyntax-only", "%f"}, "%f:%l:%c:.+: %m")
+    if linter ~= nil then
+        linter.makeLinter("precc", "prec", "precc", {"-fsyntax-only", "%f"}, "%f:%l:%c:.+: %m")
+    end
 end
